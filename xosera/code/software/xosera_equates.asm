8a41f9192a20537bc0ac6a1d0088bb49d0ca09a8
Ross Bamford <roscopeco@gmail.com>
2021-04-09 23:17:50 +0100

    New register layout for Xosera 0.11

--- xosera/code/software/xosera_equates.asm
+++ xosera/code/software/xosera_equates.asm
@@ -11,19 +11,38 @@
 ; Equates for Xosera (XVID) by Xark
 ;------------------------------------------------------------
 
+;typedef enum logic [3:0] {
+;    XVID_RD_INC,            // reg 0: read addr increment value
+;    XVID_WR_INC,            // reg 1: write addr increment value
+;    XVID_RD_MOD,            // reg 2: TODO read modulo width
+;    XVID_WR_MOD,            // reg 3: TODO write modulo width
+;    XVID_WIDTH,             // reg 4: TODO width for 2D blit
+;    XVID_RD_ADDR,           // reg 5: address to read from VRAM
+;    XVID_WR_ADDR,           // reg 6: address to write from VRAM
+;    XVID_DATA,              // reg 7: read/write word from/to VRAM RD/WR
+;    XVID_DATA_2,            // reg 8: read/write word from/to VRAM RD/WR (for 32-bit)
+;    XVID_COUNT,             // reg 9: TODO blitter "repeat" count
+;    XVID_VID_CTRL,          // reg A: TODO read status/write control settings
+;    XVID_VID_DATA,          // reg B: TODO video data (as set by VID_CTRL)
+;    XVID_AUX_RD_ADDR,       // reg C: TODO aux read address (font audio etc.?)
+;    XVID_AUX_WR_ADDR,       // reg D: TODO aux write address (font audio etc.?)
+;    XVID_AUX_CTRL,          // reg E: TODO audio and other control?
+;    XVID_AUX_DATA           // reg F: TODO aux memory/register data read/write value
+;} register_t;
+
 R_XVID_BASE         equ   $f80060       ; Xosera base address 
-R_XVID_RD_ADDR      equ   $0            ; VRAM Read Address           (WO)
-R_XVID_WR_ADDR      equ   $4            ; VRAM Write Address          (WO)
-R_XVID_DATA         equ   $8            ; VRAM Data (First word)      (RW)
-R_XVID_DATA_2       equ   $C            ; VRAM Data (Second word)     (RW)
-R_XVID_VID_CTRL     equ   $10           ; Video mode                  (WO)
-R_XVID_VID_DATA     equ   $14           ; Video mode data             (RW)
-R_XVID_RD_INC       equ   $18           ; Read address increment      (WO)
-R_XVID_WR_INC       equ   $1C           ; Write address increment     (WO)
-R_XVID_RD_MOD       equ   $20           ; Read modulo width           (WO)
-R_XVID_WR_MOD       equ   $24           ; Write modulo width          (WO)
-R_XVID_WIDTH        equ   $28           ; Width for 2D blit           (WO)
-R_XVID_BLIT_COUNT   equ   $2C           ; Blitter "repeat" count      (RW)
+R_XVID_RD_INC       equ   $0            ; Read address increment      (WO)
+R_XVID_WR_INC       equ   $4            ; Write address increment     (WO)
+R_XVID_RD_MOD       equ   $8            ; Read modulo width           (WO)
+R_XVID_WR_MOD       equ   $C            ; Write modulo width          (WO)
+R_XVID_WIDTH        equ   $10           ; Width for 2D blit           (WO)
+R_XVID_RD_ADDR      equ   $14           ; VRAM Read Address           (WO)
+R_XVID_WR_ADDR      equ   $18           ; VRAM Write Address          (WO)
+R_XVID_DATA         equ   $1C           ; VRAM Data (First word)      (RW)
+R_XVID_DATA_2       equ   $20           ; VRAM Data (Second word)     (RW)
+R_XVID_COUNT        equ   $24           ; Blitter "repeat" count      (RW)
+R_XVID_VID_CTRL     equ   $28           ; Video mode                  (WO)
+R_XVID_VID_DATA     equ   $2C           ; Video mode data             (RW)
 R_XVID_AUX_RD_ADDR  equ   $30           ; Aux read address            (WO)
 R_XVID_AUX_WR_ADDR  equ   $34           ; Aux write address           (WO)
 R_XVID_AUX_CTRL     equ   $38           ; Aux control                 (WO)
